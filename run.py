d98c8bc54f749aa7d4c6b453d8976d107e520954
estettin <elanastettin@gmail.com>
2016-11-19 17:51:05 -0600

    Try to fix things

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, session
 import twilio.twiml
+import predict
 
 
 SECRET_KEY = '357024b143141688264d414ce75447fe'
@@ -9,22 +10,23 @@ app.config.from_object(__name__)
 
 # Try adding your own number to this list!
 callers = {
-    "+15515790231": "Elana",
-    "+14158675310": "Boots",
-    "+14158675311": "Virgil",
+	"+15515790231": "Elana",
+	"+14158675310": "Boots",
+	"+14158675311": "Virgil",
 }
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond and greet the caller by name."""
+	"""Respond and greet the caller by name."""
 
-    image_url = request.values.get('MediaUrl0', None)
-
-    resp = twilio.twiml.Response()
-    resp.message(image_url)
-
-    return str(resp)
+	# image_url = "None"
+	# if request.values.get('MediaUrl0'):
+	# 	image_url = request.values.get('MediaUrl0')
+	# 	resp = twilio.twiml.Response()
+	# 	resp.message(image_url)
+	return predict.get_tags("https://static.independent.co.uk/s3fs-public/thumbnails/image/2013/01/24/12/v2-cute-cat-picture.jpg")
+	
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
